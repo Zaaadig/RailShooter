ceed9f0af277ebb9d8b5833c74b93d4031ce3364
Zaaadig <z_loyer@stu-lisaa.com>
2025-01-25 17:53:22 +0100

    suite

--- Assets/Scripts/Rail_EnemySpawner.cs
+++ Assets/Scripts/Rail_EnemySpawner.cs
@@ -13,17 +13,33 @@ public class Rail_EnemySpawner : MonoBehaviour
     [Header("Values")]
     [SerializeField] private int m_maxEnemyToSpawn = 10;
     [SerializeField] private float m_SpawnInterval = 0.5f;
+    [SerializeField] private float m_delay;
 
     private List<Rail_Enemy> m_spawnedEnemies = new List<Rail_Enemy>();
     private Coroutine m_spawnCoroutine;
+    private bool m_ready = false;
 
-    private void OnEnable()
+    private void Start()
     {
-        m_spawnCoroutine = StartCoroutine(C_Spawn());
+        StartCoroutine(C_Start());
+    }
+
+    private IEnumerator C_Start()
+    {
+        yield return new WaitForSeconds(m_delay);
+        m_ready = true;
+
+    }
+
+    private void Update()
+    {   
+        if (m_ready == true)
+            m_spawnCoroutine = StartCoroutine(C_Spawn());
     }
 
     private IEnumerator C_Spawn()
     {
+        m_ready = false;
         float timer = 0;
         float duration = m_SpawnInterval;
 
